1563278bb87bcf89f37c70c9a347b5a77ba506a5
keomony khun <keomony@gmail.com>
2017-01-18 23:05:22 +0000

    adds get file name via ARGV

--- lib/my_gem.rb
+++ lib/my_gem.rb
@@ -1,9 +1,17 @@
 require "my_gem/version"
 
 module MyGem
+
   class Greeting
-    def self.say_hello(*args)
-      puts "Hello"+args[0]
+    def self.say_hello(name)
+      puts "Hello"+name
     end
   end
+
+  class ApiIfy
+    def self.get_file_name(csv_path)
+      csv_path.split("/").last.split(".").first
+    end
+  end
+
 end
